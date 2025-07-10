da1a2d025f5c6d1e196b4eea925517a0325bd3ad
Sly-Irk <sly.irk@mail.ru>
2025-07-10 12:59:53 +0800

    First commit

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,9 +1,19 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+    private Deth8 deathless;
+
+    @Autowired
+    public void setDeth(Deth8 deathless) {
+        this.deathless = deathless;
+    }
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :( " + "";
+        return ", смерть Кощея на игле :( " + deathless.toString();
     }
 }
